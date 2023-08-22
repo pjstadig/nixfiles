20fc1499a4ab2811b7c9c2dca06d5160a3ae1041
Paul Stadig <paul@stadig.name>
2023-08-22 06:57:51 -0400

    Move machine and user config from thoughtfull

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "NixOS configuration";
+  inputs = {
+    hardware.url = "github:nixos/nixos-hardware/master";
+    nixfiles = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:thoughtfull-systems/nixfiles/main";
+    };
+    nixpkgs.url = "github:thoughtfull-systems/nixpkgs/nixos-23.05";
+    secrets.url = "git+ssh://git@github.com/thoughtfull-systems/nixfiles-secrets";
+  };
+  outputs = { hardware, nixfiles, nixpkgs, secrets, ... }@inputs: rec {
+    nixosConfigurations = {
+      hemera = nixpkgs.lib.nixosSystem {
+        modules = [
+          hardware.nixosModules.lenovo-thinkpad-x13
+          ./nixos/hemera
+        ];
+        specialArgs = {
+          secrets = secrets.hemera;
+          thoughtfull = nixfiles.nixosModules;
+        };
+        system = "x86_64-linux";
+      };
+      raspi3b = nixpkgs.lib.nixosSystem {
+        modules = [ ./nixos/raspi3b ];
+        specialArgs = {
+          secrets = secrets.raspi3b;
+          thoughtfull = nixfiles.nixosModules;
+        };
+        system = "aarch64-linux";
+      };
+      ziph = nixpkgs.lib.nixosSystem {
+        modules = [ ./nixos/ziph ];
+        specialArgs = {
+          secrets = secrets.ziph;
+          thoughtfull = nixfiles.nixosModules;
+        };
+        system = "x86_64-linux";
+      };
+    };
+  };
+}
---
+++ home/raspi3b/root.nix
@@ -0,0 +1,4 @@
+{ thoughtfull, ... }: {
+  home.stateVersion = "22.05";
+  imports = [ thoughtfull.root ];
+}
