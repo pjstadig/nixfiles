1a7ade178f2e3d95ca16ed39cad7c3c0fbb4f69e
Paul Stadig <paul@stadig.name>
2023-08-24 06:07:34 -0400

    These get set from the nixos config

--- homeManagerModules/paul.nix
+++ homeManagerModules/paul.nix
@@ -1,6 +1,5 @@
 { config, lib, ... } : {
   home = {
-    homeDirectory = lib.mkDefault "/home/${config.home.username}";
     # This value determines the Home Manager release that your
     # configuration is compatible with. This helps avoid breakage
     # when a new Home Manager release introduces backwards
@@ -10,7 +9,6 @@
     # the Home Manager release notes for a list of state version
     # changes in each release.
     stateVersion = lib.mkDefault "22.11";
-    username = lib.mkDefault "paul";
   };
   programs = {
     git = {
