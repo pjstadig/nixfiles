671bffa660b4c67bebd5a8ae02f8b254708091a5
Paul Stadig <paul@stadig.name>
2023-10-16 12:07:14 -0400

    Simplify flake inputs

--- flake.nix
+++ flake.nix
@@ -1,34 +1,15 @@
 {
   description = "NixOS configuration";
   inputs = {
-    agenix = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:ryantm/agenix/e64961977f60388dd0b49572bb0fc453b871f896";
-    };
     hardware.url = "github:nixos/nixos-hardware/master";
-    home-manager = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:thoughtfull-systems/home-manager/release-23.05";
-    };
-    nixpkgs.url = "github:thoughtfull-systems/nixpkgs/nixos-23.05";
     secrets.url = "git+ssh://git@github.com/pjstadig/nixfiles-secrets";
-    thoughtfull = {
-      inputs = {
-        agenix.follows = "agenix";
-        home-manager.follows = "home-manager";
-        nixpkgs.follows = "nixpkgs";
-        unstable.follows = "unstable";
-      };
-      url = "github:thoughtfull-systems/nixfiles/main";
-      # url = "/home/paul/src/thoughtfull/nixfiles";
-    };
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # thoughtfull.url = "github:thoughtfull-systems/nixfiles/main";
+    thoughtfull.url = "/home/paul/src/thoughtfull/nixfiles";
   };
-  outputs = { hardware, nixpkgs, secrets, self, thoughtfull, ... }@inputs: {
-    inherit thoughtfull;
+  outputs = { hardware, secrets, self, thoughtfull, ... }@inputs: {
     homeManagerModules = import ./homeManagerModules;
     nixosConfigurations = {
-      hemera = nixpkgs.lib.nixosSystem {
+      hemera = thoughtfull.inputs.nixpkgs.lib.nixosSystem {
         modules = [
           hardware.nixosModules.lenovo-thinkpad-x13
           ./nixos/hemera
@@ -40,7 +21,7 @@
         };
         system = "x86_64-linux";
       };
-      raspi3b = nixpkgs.lib.nixosSystem {
+      raspi3b = thoughtfull.inputs.nixpkgs.lib.nixosSystem {
         modules = [ ./nixos/raspi3b ];
         specialArgs = {
           secrets = secrets.raspi3b;
@@ -49,7 +30,7 @@
         };
         system = "aarch64-linux";
       };
-      ziph = nixpkgs.lib.nixosSystem {
+      ziph = thoughtfull.inputs.nixpkgs.lib.nixosSystem {
         modules = [ ./nixos/ziph ];
         specialArgs = {
           secrets = secrets.ziph;
