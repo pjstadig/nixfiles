c9260c03fca4fda81ec91f8f989a88c8c1818583
Paul Stadig <paul@stadig.name>
2023-10-24 10:02:25 -0400

    Add unstable input

--- flake.nix
+++ flake.nix
@@ -3,12 +3,15 @@
   inputs = {
     hardware.url = "github:nixos/nixos-hardware/master";
     nixpkgs.url = "github:thoughtfull-systems/nixpkgs/nixos-23.05";
+    # nixpkgs.url = "/home/paul/src/thoughtfull/nixpkgs";
     secrets.url = "git+ssh://git@github.com/pjstadig/nixfiles-secrets";
     thoughtfull = {
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.unstable.follows = "unstable";
       url = "github:thoughtfull-systems/nixfiles/main";
       # url = "/home/paul/src/thoughtfull/nixfiles";
     };
+    unstable.url = "github:thoughtfull-systems/nixpkgs/nixos-unstable";
   };
   outputs = { hardware, nixpkgs, secrets, self, ... }@inputs: {
     homeManagerModules = import ./homeManagerModules;
