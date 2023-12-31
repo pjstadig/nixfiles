a6103e8c231b37450b6cc913946a0be37cafa1f2
Paul Stadig <paul@stadig.name>
2023-12-31 16:11:03 -0500

    Run firefox and terminal in dedicated workspace

--- homeManagerModules/init.el
+++ homeManagerModules/init.el
@@ -8,26 +8,31 @@
 
 ;;; Code:
 (require 'use-package)
-(use-package org
-  :after pjs-gtd)
+(use-package org)
 (use-package pjs-gtd
-  :after pjs
+  :after pjs-org
   :bind ("C-c g c" . pjs-gtd-capture)
   :demand t
   :hook (org-mode . pjs-gtd-set-regexp))
 (use-package pjs-gtd-agenda
-  :bind ("C-c g d" . pjs-gtd-agenda-daily))
+  :after pjs-gtd
+  :bind ("C-c g d" . (lambda ()
+                       (interactive)
+                       (exwm-workspace-switch 6)
+                       (pjs-gtd-agenda-daily)
+                       (delete-other-windows))))
 (use-package pjs-gtd-process
   :bind ("C-c g p" . pjs-gtd-process))
 (use-package pjs-javascript)
 (use-package pjs-ol-obsidian)
-(use-package pjs-org)
+(use-package pjs-org
+  :after (pjs org))
 (use-package pjs-org-bullets
-  :after (org pjs-org))
+  :after pjs-org)
 (use-package pjs-org-capture
-  :after (org pjs-org))
+  :after pjs-org)
 (use-package pjs-org-faces
-  :after (org pjs-org))
+  :after pjs-org)
 
 (provide 'init)
 ;;; init.el ends here
