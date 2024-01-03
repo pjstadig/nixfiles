69a5e43f6d117f33c12ef8bb7611b119405bcf09
Paul Stadig <paul@stadig.name>
2024-01-03 15:57:17 -0500

    pjs->tfl

--- homeManagerModules/init.el
+++ homeManagerModules/init.el
@@ -9,30 +9,30 @@
 ;;; Code:
 (require 'use-package)
 (use-package org)
-(use-package pjs-gtd
-  :after pjs-org
-  :bind ("C-c g c" . pjs-gtd-capture)
+(use-package tfl-gtd
+  :after tfl-org
+  :bind ("C-c g c" . tfl-gtd-capture)
   :demand t
-  :hook (org-mode . pjs-gtd-set-regexp))
-(use-package pjs-gtd-agenda
-  :after pjs-gtd
+  :hook (org-mode . tfl-gtd-set-regexp))
+(use-package tfl-gtd-agenda
+  :after tfl-gtd
   :bind ("C-c g d" . (lambda ()
                        (interactive)
                        (exwm-workspace-switch 6)
-                       (pjs-gtd-agenda-daily)
+                       (tfl-gtd-agenda-daily)
                        (delete-other-windows))))
-(use-package pjs-gtd-process
-  :bind ("C-c g p" . pjs-gtd-process))
-(use-package pjs-javascript)
-(use-package pjs-ol-obsidian)
-(use-package pjs-org
-  :after (pjs org))
-(use-package pjs-org-bullets
-  :after pjs-org)
-(use-package pjs-org-capture
-  :after pjs-org)
-(use-package pjs-org-faces
-  :after pjs-org)
+(use-package tfl-gtd-process
+  :bind ("C-c g p" . tfl-gtd-process))
+(use-package tfl-javascript)
+(use-package tfl-ol-obsidian)
+(use-package tfl-org
+  :after (tfl org))
+(use-package tfl-org-bullets
+  :after tfl-org)
+(use-package tfl-org-capture
+  :after tfl-org)
+(use-package tfl-org-faces
+  :after tfl-org)
 
 (provide 'init)
 ;;; init.el ends here
--- homeManagerModules/paul.nix
+++ homeManagerModules/paul.nix
@@ -2,12 +2,12 @@
   home.file.".config/emacs/init.el".source = ./init.el;
   programs = {
     emacs.extraPackages = epkgs: with epkgs; [
-      pjs-gtd
-      pjs-ol-obsidian
-      pjs-org
-      pjs-org-bullets
-      pjs-org-capture
-      pjs-org-faces
+      tfl-gtd
+      tfl-ol-obsidian
+      tfl-org
+      tfl-org-bullets
+      tfl-org-capture
+      tfl-org-faces
     ];
     git = {
       enable = true;
